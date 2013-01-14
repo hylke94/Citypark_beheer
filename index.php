aa2b39527855386d7bb9bc847f6a19fdc6c69206
Pim Vellinga <pimvellinga@gmail.com>
2013-01-14 01:00:10 +0100

    Dynamic page loading

--- index.php
+++ index.php
@@ -3,12 +3,11 @@ error_reporting(E_ALL);
 print_r($_POST);
 require_once 'libs/Smarty.class.php';
 require_once 'functions.php';
+
 $smarty =  new Smarty();
 
 $smarty->compile_check = true;
 $smarty->caching = false;
-
-$smarty->display('templates/index.tpl');
 
 
 if($_SERVER['REQUEST_METHOD'] == "POST"){
@@ -18,15 +17,15 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 	
 }
 
-// if(!isset($_GET['page'])){
-// 	$smarty->display("templates/home.tpl");
-// }
-// else {
-// 	if (file_exists("templates/".$_GET["page"].".tpl")){
-// 		$smarty->display("templates/".$_GET["page"].".tpl");
-// 	}
-// 	else {
-// 		$smarty->display("templates/home.tpl");
-// 	}
-// }
+if(!isset($_GET['page'])){
+	$smarty->display("templates/index.tpl");
+}
+else {
+	if (file_exists("templates/".$_GET["page"].".tpl")){
+		$smarty->display("templates/".$_GET["page"].".tpl");
+	}
+	else {
+		$smarty->display("templates/index.tpl");
+	}
+}
 ?>
