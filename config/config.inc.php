326e2bb54c00efa4b0b4b0f80a2cb10114d606a0
Pim Vellinga <pimvellinga@gmail.com>
2013-01-26 01:08:49 +0100

    Host should be first

--- config/config.inc.php
+++ config/config.inc.php
@@ -1,8 +1,13 @@
 <?php
 
+/**
+ * Enter the credentials for the database
+ * 
+ */
+
+define("MYSQL_HOST","localhost");
 define("MYSQL_USER","root");
 define("MYSQL_PASS","");
 define("MYSQL_DB","citypark_beheer");
-define("MYSQL_HOST","localhost");
 
 ?>
